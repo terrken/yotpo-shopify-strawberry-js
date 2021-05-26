8bdbcdcfd872da240153c98d25a88100706ab058
terrken <79187268+terrken@users.noreply.github.com>
2021-05-27 05:46:02 +0800

    removed my app key

--- strawberry.js
+++ strawberry.js
@@ -4,7 +4,7 @@ var strawberryapp = angular.module('strawberry', ['ngSanitize']).config(function
 
 strawberryapp.controller('main', function($templateRequest, $sce, $compile, $scope, $http, $location){
   var strawberryRender=function(e,t,a,c){$scope.data=[],$templateRequest($sce.getTrustedResourceUrl(t)).then(function(t){void 0!==c&&angular.forEach(c,function(e,t){$scope.data[t]=e}),$compile($("#"+e).append(t))($scope)})};
-  var yotpoApi = "https://api.yotpo.com/v1/widget/4WbfkD5arSq1IIQLEwVzMLn7hnXNKCVSPNCpxgcy/products/"+shopifyProductID+"/reviews.json";
+  var yotpoApi = "https://api.yotpo.com/v1/widget/{{ YOUR_YOTPO_APP_KEY }}/products/"+shopifyProductID+"/reviews.json";
   $http.get(yotpoApi).then(function(response){
     if( response.status == 200){strawberryRender("main-yotpo-widget", "yotpo-widget.htm", "yotpo", response.data.response);}
   });
