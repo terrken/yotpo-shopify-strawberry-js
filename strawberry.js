0b157bbd4853c9446191c9bdea1d0f6882f6ca17
Ken Terrado <79209076+rterrado@users.noreply.github.com>
2021-05-27 04:06:37 +0800

    strawberry js

---
+++ strawberry.js
@@ -0,0 +1,11 @@
+var strawberryapp = angular.module('strawberry', ['ngSanitize']).config(function ($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+});
+
+strawberryapp.controller('main', function($templateRequest, $sce, $compile, $scope, $http, $location){
+  var strawberryRender=function(e,t,a,c){$scope.data=[],$templateRequest($sce.getTrustedResourceUrl(t)).then(function(t){void 0!==c&&angular.forEach(c,function(e,t){$scope.data[t]=e}),$compile($("#"+e).append(t))($scope)})};
+  var yotpoApi = "https://api.yotpo.com/v1/widget/4WbfkD5arSq1IIQLEwVzMLn7hnXNKCVSPNCpxgcy/products/"+shopifyProductID+"/reviews.json";
+  $http.get(yotpoApi).then(function(response){
+    if( response.status == 200){strawberryRender("main-yotpo-widget", "yotpo-widget.htm", "yotpo", response.data.response);}
+  });
+});
